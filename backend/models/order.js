5a9fc7c91268ca75245c1f1f14e615a1867feaa7
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-11 11:40:15 +0530

    Order Model Initialize

---
+++ backend/models/order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { userDb } = require("../db");
+
+// Order Schema
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+      {
+          productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+          title: String,
+          price: Number,
+          quantity: Number,
+          image: String,
+          category: String, 
+      }
+  ],
+  totalPrice: Number,
+  orderId: { type: String, unique: true }, 
+  paymentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Payment', required: true },
+  shippingAddress: {
+    address: { type: String },
+    city: { type: String },
+    state: { type: String },
+    pincode: { type: String },
+    country: { type: String },
+    mobileno: { type: String },
+  },
+  status: { 
+    type: String, 
+    enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'], 
+    default: 'Pending' 
+  },
+  deliveryDate: { type: Date },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Order = userDb.model("Order", orderSchema, "order");
+
+module.exports = Order;
