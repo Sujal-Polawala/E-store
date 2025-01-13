8d6fe4fa82a9f40e2ee76059111d4efbfabe9b99
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-13 16:24:44 +0530

    Update User File

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,8 +7,6 @@ const UserSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  badge: { type: String, default: false} ,
-  color: { type: String } ,
   password: { type: String, required: true },
   confirmPassword: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
