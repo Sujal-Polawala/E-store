2dc3aaf88d27ff497825e8770e2332622e2aed43
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-29 11:15:40 +0530

    Update Product Structure

--- client/src/components/home/Products/Product.js
+++ client/src/components/home/Products/Product.js
@@ -29,7 +29,7 @@ const Product = (props) => {
     });
   };
 
-  console.log(productInfo);
+  // console.log(productInfo);
 
   const handleAddToCart = async () => {
     try {
@@ -64,7 +64,7 @@ const Product = (props) => {
       <div className="w-full relative group">
         <div className="max-w-80 max-h-80 relative overflow-y-hidden">
           <div>
-            <Image className="w-full h-52" imgSrc={props.img} />
+            <Image className="w-full h-52" imgSrc={props.image} />
           </div>
           <div className="absolute top-6 left-8">
             {props.badge && <Badge text={props.badge} />}
@@ -106,7 +106,7 @@ const Product = (props) => {
         </div>
         <div className="max-w-80 py-6 flex flex-col gap-1 border-[1px] border-t-0 px-4">
           <div className="flex items-center justify-between font-titleFont">
-            <h2 className="text-lg text-primeColor font-bold">{props.title}</h2>
+            <h2 className="text-lg text-primeColor font-bold">{props.title.slice(0,20)}...</h2>
             <p className="text-[#767676] text-[14px]">${props.price}</p>
           </div>
           <div>
