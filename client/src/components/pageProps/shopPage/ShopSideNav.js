18ce71c5a492312ccc3ff0e9c8ab3c111d97df33
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-29 11:25:47 +0530

    Update ShopSideNav

--- client/src/components/pageProps/shopPage/ShopSideNav.js
+++ client/src/components/pageProps/shopPage/ShopSideNav.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Brand from "./shopBy/Brand";
 import Category from "./shopBy/Category";
 import Color from "./shopBy/Color";
+import Brand from "./shopBy/Brand";
 import Price from "./shopBy/Price";
 
-const ShopSideNav = () => {
+const ShopSideNav = ({ onCategoryFilter, clearFilters }) => {
   return (
     <div className="w-full flex flex-col gap-6">
-      <Category icons={false} />
-      <Color />
-      <Brand />
+      <Category onCategoryFilter={onCategoryFilter} clearFilters={clearFilters} />
+      {/* <Color /> */}
+      {/* <Brand /> */}
       <Price />
     </div>
   );
