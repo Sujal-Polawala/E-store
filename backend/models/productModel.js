eae4b41928517ad444635d0ffd1c176bb1b04a48
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-29 10:41:30 +0530

    Update Model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,28 +1,4 @@
 // // /models/productModel.js
-
-// const mongoose = require('mongoose');
-
-// const productSchema = new mongoose.Schema(
-//   {
-//     id: { type: Number, required: true},
-//     name: { type: String, required: true },
-//     price: { type: Number, required: true },
-//     image: { type: String, required: true },
-//     color: { type: String },
-//     description: { type: String },
-//     category: { type: String, enum: ['Mens Clothing', 'Womens Clothing', 'Watches', 'Jewelry', 'Accessories'], required: true },
-//     // isNewArrival: { type: Boolean, default: false },
-//     // isSpecialOffer: { type: Boolean, default: false },
-//     // isBestSeller: { type: Boolean, default: false },
-//     badge: { type: Boolean, default: false },
-//     qty: { type: Number, required: true},
-//   },
-//   { timestamps: true }
-// );
-
-// const Product = mongoose.model('Product', productSchema);
-
-// module.exports = Product;
 const mongoose = require("mongoose");
 
 // Define the product schema
