f52332624830167856b94d95335acfb8b59d0b9f
Sujal-Polawala <sujalpolawala@gmail.com>
2025-01-29 10:35:13 +0530

    Add Category CRUD Operation

---
+++ backend/controllers/category/categoryController.js
@@ -0,0 +1,57 @@
+const Category = require("../../models/category");
+
+// Add a new category
+
+exports.addCategory = async (req, res) => {
+  try {
+    const { name, description, icons } = req.body;
+    const newCategory = new Category({ name, description, icons });
+    await newCategory.save();
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(400).json({ error: "Failed to add category" });
+  }
+};
+
+// Update a category
+exports.updateCategory = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const { name, description, icons } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send("No category with that ID");
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      _id,
+      { name, description, icons },
+      { new: true }
+    );
+
+    if (!updatedCategory) return res.status(404).send("No category found");
+
+    res.json(updatedCategory);
+  } catch (err) {
+    res.status(400).json({ error: "Failed to update category" });
+  }
+};
+
+// Delete a category
+exports.deleteCategory = async (req, res) => {
+  try {
+    await Category.findByIdAndDelete(req.params.id);
+    res.json({ message: "Category deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete category" });
+  }
+};
+
+// Get all categories
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to get categories" });
+  }
+};
