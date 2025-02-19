8b66c242506ebf0c4811298b2b5e25f105c4d7b1
Sujal-Polawala <sujalpolawala@gmail.com>
2025-02-19 11:26:54 +0530

    Add Schema for GraphQL

---
+++ backend/graphql/schema.js
@@ -0,0 +1,35 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Order {
+    _id: ID!
+    totalPrice: Float!
+    createdAt: String!
+    sellerId: ID!
+  }
+
+  type Query {
+    orders(sellerId: ID!): [Order]
+    totalOrders: Int
+    totalRevenue: Float
+    totalUsers: Int
+    totalSellers: Int
+    sellers: [Seller]
+    bestSellingProducts: [Product]
+  }
+
+  type Seller {
+    _id: ID!
+    name: String!
+    orders: [Order] 
+    totalSales: Float
+  }
+
+  type Product {
+    _id: ID!
+    title: String!
+    sales: Int!
+  }
+`;
+
+module.exports = typeDefs;
