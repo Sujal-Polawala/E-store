2525941b18d25bf052b57f2cf264e6779e6be3fc
Sujal-Polawala <sujalpolawala@gmail.com>
2025-02-19 11:56:42 +0530

    Add Price Filtering

--- client/src/components/pageProps/shopPage/ShopSideNav.js
+++ client/src/components/pageProps/shopPage/ShopSideNav.js
@@ -4,13 +4,13 @@ import Color from "./shopBy/Color";
 import Brand from "./shopBy/Brand";
 import Price from "./shopBy/Price";
 
-const ShopSideNav = ({ onCategoryFilter, clearFilters }) => {
+const ShopSideNav = ({ onCategoryFilter, clearFilters, onPriceFilter }) => {
   return (
     <div className="w-full flex flex-col gap-6">
       <Category onCategoryFilter={onCategoryFilter} clearFilters={clearFilters} />
       {/* <Color /> */}
-      {/* <Brand /> */}
-      <Price />
+      <Brand />
+      <Price onPriceFilter={onPriceFilter} clearFilters={clearFilters} />
     </div>
   );
 };
